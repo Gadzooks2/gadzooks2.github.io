cdfa4e11d42e93bbf0b5f928e26057db449ff071
Gadzooks2 <bloopy812@gmail.com>
2019-05-12 13:13:52 +0100

    Update 2048-ai.js

--- 2048-ai.js
+++ 2048-ai.js
@@ -334,14 +334,14 @@ function setup_UI() {
 	div.appendChild(p);
 	div.appendChild(p2);	
 	var input = document.createElement('input');
-  var input_box = div.appendChild(input);
-  input_box.setAttribute("type", "text");
-  input_box.setAttribute("id", "numberOfAIs");
-  input_box.setAttribute("onchange", "setAIs();");
-  input_box.setAttribute("onkeypress", "this.onchange();");
-  input_box.setAttribute("onpaste", "this.onchange();");
-  input_box.setAttribute("oninput", "this.onchange();");
-
+  	var input_box = div.appendChild(input);
+  	input_box.setAttribute("type", "text");
+  	input_box.setAttribute("id", "numberOfAIs");
+  	input_box.setAttribute("onchange", "setAIs();");
+  	input_box.setAttribute("onkeypress", "this.onchange();");
+  	input_box.setAttribute("onpaste", "this.onchange();");
+  	input_box.setAttribute("oninput", "this.onchange();");
+	
   function setAIs(){
 	  AIs = Number(document.getElementById("numberOfAIs").value);
 	  RUNS = AIs;
